cc2e179dd071591b8dc4c0433d1fbc2ad6f82130
tap222 <mtapas.mohanty95@gmail.com>
2017-05-17 23:59:02 +0530

    Update data_model.R

--- data_model.R
+++ data_model.R
@@ -17,28 +17,34 @@ df$timestamp<-anytime(df$timestamp)
 cnt_val= 0
 cnt_inval= 0
 cnt_mul_ses = 0
-avg_time <- vector()
-gameid<- vector()
-game_ttl <- vector()
-game_non_cons <- vector()
+avg_time <- data.frame("time" = integer(), stringsAsFactors=FALSE)
+gameid<- data.frame("gameid" = integer(), stringsAsFactors=FALSE)
+game_ttl <- data.frame("gameid" = integer(), stringsAsFactors=FALSE)
+game_non_cons <- data.frame("gameid" = integer(), stringsAsFactors=FALSE)
 
 for (i in 1:(nrow(df)-1)) { 
   if((df[i+1,"ai5"]==df[i,"ai5"]) && (df[i+1,"gameid"]==df[i,"gameid"]))
     if (df[i,"eventtype"] == "ggstart" && df[i+1,"eventtype"]== "ggstop")
       if (as.numeric(df[i+1,"timestamp"]-df[i,"timestamp"]) >= 60){
-        data.frame(append(gameid,df[i+1,"gameid"]))
-        data.frame(append(avg_time,as.numeric(df[i,"timestamp"]-df[i+1,"timestamp"])))
+        gameid[nrow(gameid) + 1, ] <- c( df[i+1,"gameid"])
+        avg_time[nrow(avg_time) + 1, ] <- c(df[i+1,"timestamp"]-df[i,"timestamp"])
         inc(cnt_val)=1 }
-  else if ((as.numeric(df[i+1,"timestamp"],df[i,"timestamp"])) >= 30) {
-    data.frame(append(gameid,df[i+1,"gameid"]))
-    data.frame(append(avg_time,as.numeric((df[i,"timestamp"]-df[i+1,"timestamp"]))))
-    inc(cnt_mul_ses)=1 }
-  else if (as.numeric(df[i+1,"timestamp"]-df[i,"timestamp"]) < 30) {
-    data.frame(append(game_ttl,df[i+1,"gameid"]))
-    inc(cnt_inval)= 1 }
-  else if (df[i,"eventtype"] == "ggstart" && df[i+1,"eventtype"]== "ggstart"){
-    data.frame(append(game_ttl,df[i+1,"gameid"]))
-    inc(cnt_inval)= 1 }
+  else {
+        if ((as.numeric(df[i+1,"timestamp"],df[i,"timestamp"])) >= 30) 
+          gameid[nrow(gameid) + 1, ] <- c( df[i+1,"gameid"])
+          avg_time[nrow(avg_time) + 1, ] <- c(df[i+1,"timestamp"]-df[i,"timestamp"])
+           inc(cnt_mul_ses)=1 
+             }
+  else {
+        if (as.numeric(df[i+1,"timestamp"]-df[i,"timestamp"]) < 30) 
+           game_ttl[nrow(game_ttl) + 1, ] <- c( df[i+1,"gameid"])
+           inc(cnt_inval)= 1 
+            }
+  else {
+     if (df[i,"eventtype"] == "ggstart" && df[i+1,"eventtype"]== "ggstart"){
+        game_ttl[nrow(game_ttl) + 1, ] <- c( df[i+1,"gameid"])
+        inc(cnt_inval)= 1 
+         }
   else
     continue }
 
